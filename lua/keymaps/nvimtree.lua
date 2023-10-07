55c19023f33888a2b0112979a81be4119b6dc347
Toni Lubiniecki <toni@lubiniecki.de>
2023-10-07 14:27:51 +0200

    DEL: old files

--- lua/keymaps/nvimtree.lua
+++
@@ -1,6 +0,0 @@
-local options = { silent = true, noremap = true }
-local function set(mode, lhs, rhs, opts)
-    vim.keymap.set(mode, lhs, rhs, opts)
-end
-
-set("n", "<leader>et", ":NvimTreeToggle<CR>", options)
--- lua/lazy-vim.lua
+++ lua/lazy-vim.lua
@@ -5,49 +5,49 @@ global.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-	install = { missing = true },
-	defaults = { lazy = true },
-	change_detection = { enabled = true },
-	checker = { enabled = true },
-	debug = false,
-	ui = {
-		size = { width = 0.5, height = 0.5 },
-		wrap = true,
-		border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-		icons = {
-			cmd = " ",
-			config = "",
-			event = "",
-			ft = " ",
-			init = " ",
-			import = " ",
-			keys = " ",
-			lazy = "󰒲 ",
-			loaded = "●",
-			not_loaded = "○",
-			plugin = " ",
-			runtime = " ",
-			source = " ",
-			start = "",
-			task = "✔ ",
-			list = {
-				"●",
-				"➜",
-				"★",
-				"‒",
-			},
-		},
-	},
+    install = { missing = true },
+    defaults = { lazy = true },
+    change_detection = { enabled = true },
+    checker = { enabled = true },
+    debug = false,
+    ui = {
+        size = { width = 0.5, height = 0.5 },
+        wrap = true,
+        border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        icons = {
+            cmd = " ",
+            config = "",
+            event = "",
+            ft = " ",
+            init = " ",
+            import = " ",
+            keys = " ",
+            lazy = "󰒲 ",
+            loaded = "●",
+            not_loaded = "○",
+            plugin = " ",
+            runtime = " ",
+            source = " ",
+            start = "",
+            task = "✔ ",
+            list = {
+                "●",
+                "➜",
+                "★",
+                "‒",
+            },
+        },
+    },
 })
