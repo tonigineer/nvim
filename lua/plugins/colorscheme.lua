c93ffb331b2eafcdc5572c522493ea5a741b012b
Z790E_WIN11 <toni@lubiniecki.de>
2023-11-28 20:48:24 +0100

    chg: colorscheme material ocean deep

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,34 +1,73 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    'marko-cerovac/material.nvim',
+    lazy = false,
+    priority = 1000,
 
     config = function()
-        require("tokyonight").setup({
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            style = "moon",         -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-            light_style = "day",    -- The theme is used when the background is set to light
-            transparent = true,     -- Enable this to disable setting the background color
-            terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-            styles = {
-                -- Style to be applied to different syntax groups
-                -- Value is any valid attr-list value for `:help nvim_set_hl`
-                comments = { italic = true },
-                keywords = { italic = false },
-                functions = {},
-                variables = {},
-                -- Background styles. Can be "dark", "transparent" or "normal"
-                sidebars = "transparent",     -- style for sidebars, see below
-                floats = "transparent",       -- style for floating windows
+        require('material').setup({
+            contrast = {
+                terminal = false,            -- Enable contrast for the built-in terminal
+                sidebars = false,            -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+                floating_windows = false,    -- Enable contrast for floating windows
+                cursor_line = false,         -- Enable darker background for the cursor line
+                non_current_windows = false, -- Enable contrasted background for non-current windows
+                filetypes = {},              -- Specify which filetypes get the contrasted (darker) background
             },
-            sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-            day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-            hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-            dim_inactive = true,              -- dims inactive windows
-            lualine_bold = false,             -- When `true`, section headers in the lualine theme will be bold
+            styles = {                       -- Give comments style such as bold, italic, underline etc.
+                comments = { --[[ italic = true ]] },
+                strings = { --[[ bold = true ]] },
+                keywords = { --[[ underline = true ]] },
+                functions = { --[[ bold = true, undercurl = true ]] },
+                variables = {},
+                operators = {},
+                types = {},
+            },
+            plugins = { -- Uncomment the plugins that you use to highlight them
+                -- Available plugins:
+                -- "dap",
+                -- "dashboard",
+                -- "eyeliner",
+                -- "fidget"
+                -- "flash"
+                "gitsigns",
+                -- "harpoon",
+                -- "hop",
+                "illuminate",
+                "indent-blankline",
+                "lspsaga",
+                -- "mini",
+                -- "neogit",
+                -- "neotest",
+                -- "neorg",
+                -- "noice"
+                -- "nvim-cmp",
+                -- "nvim-navic",
+                "nvim-tree",
+                "nvim-web-devicons",
+                "rainbow-delimiters",
+                -- "sneak",
+                "telescope",
+                -- "trouble",
+                "which-key",
+            },
+            disable = {
+                colored_cursor = true, -- Disable the colored cursor
+                borders = false,       -- Disable borders between verticaly split windows
+                background = false,    -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
+                term_colors = false,   -- Prevent the theme from setting terminal colors
+                eob_lines = false      -- Hide the end-of-buffer lines
+            },
+            high_visibility = {
+                lighter = false,       -- Enable higher contrast text for lighter style
+                darker = false         -- Enable higher contrast text for darker style
+            },
+            lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+            async_loading = true,      -- Load parts of the theme asyncronously for faster startup (turned on by default)
+            custom_colors = nil,       -- If you want to override the default colors, set this to a function
+            custom_highlights = {},    -- Overwrite highlights with your own
         })
 
-        vim.cmd("colorscheme tokyonight")
+        vim.g.material_style = "deep ocean"
+        vim.cmd("colorscheme material")
     end
 }
