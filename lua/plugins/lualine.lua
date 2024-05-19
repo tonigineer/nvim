591e3d517e7091755ae539b2a685ef3e107b8810
Z790E_WIN11 <toni@lubiniecki.de>
2024-05-20 00:36:50 +0200

    neotree options added

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -187,20 +187,20 @@ return {
         ins_left({
             -- Lsp server name .
             function()
-                local msg = "No Active Lsp"
+                local msg = ""
                 local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
                 local clients = vim.lsp.get_active_clients()
                 if next(clients) == nil then return msg end
                 for _, client in ipairs(clients) do
                     local filetypes = client.config.filetypes
                     if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                        return client.name
+                        return "󰚔 " .. client.name
                     end
                 end
                 return msg
             end,
-            icon = " LSP:",
-            color = { fg = colors.green, gui = "bold" },
+            icon = "",
+            color = { fg = colors.green },
         })
 
         -- Add components to right sections
