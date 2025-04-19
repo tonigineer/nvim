40aaa6d323026a656de01644e231d874c8b5cfcc
Toni Lubiniecki <hidden.toni@pm.me>
2025-04-19 16:41:54 +0200

    obsolete hyprls cmd commented out

--- lua/tonigineer/autocmds.lua
+++ lua/tonigineer/autocmds.lua
@@ -1,69 +1,69 @@
 -- Hide stuff
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {
-    "help",
-    "alpha",
-    "dashboard",
-    "neo-tree",
-    "Trouble",
-    "trouble",
-    "lazy",
-    "mason",
-    "notify",
-    "toggleterm",
-    "lazyterm",
-  },
-  callback = function()
-    vim.b.miniindentscope_disable = true
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
-  end,
+    pattern = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+    },
+    callback = function()
+        vim.b.miniindentscope_disable = true
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+    end,
 })
 
--- Hyprlang LSP
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-  pattern = { "*.hl", "hypr*.conf" },
-  callback = function(event)
-    print(string.format("starting hyprls for %s", vim.inspect(event)))
-    vim.lsp.start({
-      name = "hyprlang",
-      cmd = { "hyprls" },
-      root_dir = vim.fn.getcwd(),
-    })
-  end,
-})
+-- -- Hyprlang LSP
+-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+--   pattern = { "*.hl", "hypr*.conf" },
+--   callback = function(event)
+--     print(string.format("starting hyprls for %s", vim.inspect(event)))
+--     vim.lsp.start({
+--       name = "hyprlang",
+--       cmd = { "hyprls" },
+--       root_dir = vim.fn.getcwd(),
+--     })
+--   end,
+-- })
 
 -- Style inlay hints
 vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#868386", italic = true })
 
 -- Enable inlay hints
 vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = false,
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = false,
 })
 
 -- Enable hover of diagnostic window when in line
 vim.api.nvim_create_autocmd("CursorHold", {
-  buffer = bufnr,
-  callback = function()
-    local opts = {
-      focusable = false,
-      close_events = {
-        "BufLeave",
-        "CursorMoved",
-        "InsertEnter",
-        "FocusLost",
-      },
-      border = "rounded",
-      source = "always",
-      prefix = " ",
-      scope = "cursor",
-    }
-    vim.diagnostic.open_float(nil, opts)
-  end,
+    buffer = bufnr,
+    callback = function()
+        local opts = {
+            focusable = false,
+            close_events = {
+                "BufLeave",
+                "CursorMoved",
+                "InsertEnter",
+                "FocusLost",
+            },
+            border = "rounded",
+            source = "always",
+            prefix = " ",
+            scope = "cursor",
+        }
+        vim.diagnostic.open_float(nil, opts)
+    end,
 })
 
 -- Disable Treesitter highlighting globally
